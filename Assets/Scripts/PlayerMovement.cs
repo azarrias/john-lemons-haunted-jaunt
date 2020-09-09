4df68e4703c97515bc46eed80c6a2f972d09d522
azarrias <adolfo.zarrias@gmail.com>
2020-09-09 13:05:15 +0200

    Apply movement and rotation to PC rigidbody

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float turnSpeed = 20f;
     Animator m_Animator;
+    Rigidbody m_Rigidbody;
     Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
 
     void Start()
     {
         m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
@@ -26,5 +30,17 @@ public class PlayerMovement : MonoBehaviour
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("IsWalking", isWalking);
+
+        // make the character face its direction of movement
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    // apply rotation as well as the root motion movement
+    private void OnAnimatorMove()
+    {
+        // move by the magnitude of the change due to root motion in the movement vector direction
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
