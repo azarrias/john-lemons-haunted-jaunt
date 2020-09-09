8a3a429988df6dde49d5a1b2ddf147510c2ed789
azarrias <adolfo.zarrias@gmail.com>
2020-09-09 12:40:53 +0200

    Add PlayerMovement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator m_Animator;
+    Vector3 m_Movement;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        // keep PC speed consistent for diagonal movement
+        m_Movement.Normalize();
+
+        // tell the animator component whether the PC should be walking or not
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+    }
+}
