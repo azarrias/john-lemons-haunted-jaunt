9e165770d19c6a3b9de12d0a750aaa2e9c70b14f
azarrias <adolfo.zarrias@gmail.com>
2020-09-09 20:55:41 +0200

    Setup ending game trigger and fade out when the PC completes the level and escapes

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    // ensure that the game doesn't end before the fade has finished
+    float m_Timer;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // make sure that ending is only triggered when the PC hits the box collider
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            m_Timer += Time.deltaTime;
+            exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+            if (m_Timer > fadeDuration + displayImageDuration)
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
